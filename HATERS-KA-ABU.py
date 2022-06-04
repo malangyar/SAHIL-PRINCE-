2032f9bd2549d76592ddbe6db59a7f84d7a0f4d5
Sahil-Prince <106463378+malangyar@users.noreply.github.com>
2022-06-04 12:29:55 +0500

    Update HATERS-KA-ABU.py

--- HATERS-KA-ABU.py
+++ HATERS-KA-ABU.py
@@ -6,4 +6,4 @@ print("\033[1;32m JALNE WALO JALNA NHI")
 print("\033[1;32m  This Tool Is Cooded By Sahil-Prince")
 print("\033[1;31m  Haters Papa Bolne Ka Liye Ready Ho Jao")
 import JANI
-Main()
+Subscraption()
